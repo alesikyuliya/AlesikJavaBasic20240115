b09421eb77996cc1f8fb86a9a33b99478cf97ff5
Yuliya Alesik <alesikyuliya@outlook.com>
2024-01-15 13:57:46 +0100

    First commit

--- src/Homework8.java
+++ src/Homework8.java
@@ -2,7 +2,6 @@ public class Homework8 {
 
     public static void main(String[] args) {
         System.out.println("Hello world from Julia to Git");
-        System.out.println("Second commit to Git");
     }
 
 
